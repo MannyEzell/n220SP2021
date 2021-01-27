f833e49e68476321ae3ff61a81e45dc54cd585d9
MannyEzell <78098770+MannyEzell@users.noreply.github.com>
2021-01-27 16:33:33 -0500

    Personal Composition added

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,15 @@
+function setup(){
+    var skyColor = "#00FFFF";
+    var groundColor = "#7EC850"
+    createCanvas(400,400);
+
+    background(skyColor);
+    fill(255,0,0);
+    triangle(200,110,175,160,225,160)
+    line(100,110,300,110);
+    fill(0,255,0);
+    ellipse(100,85,50,50);    
+    ellipse(300,85,50,50);
+    fill(groundColor);
+    rect(0,160,400,240,) 
+}
